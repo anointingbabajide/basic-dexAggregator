7f1de9936915f792b8e39d81ddb8bb631be17615
mojowlekaz <anointingbabajide@gmail.com>
2025-07-16 21:20:45 +0100

    :

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -15,6 +17,17 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  webpack(config) {
+    config.optimization.minimizer = config.optimization.minimizer.map(
+      (plugin) => {
+        if (plugin.constructor.name === "TerserPlugin") {
+          plugin.options.exclude = /HeartbeatWorker/;
+        }
+        return plugin;
+      }
+    );
+    return config;
   },
 };
 
